765571a964d6d28b72b1976fd0b61ede6901b18f
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2024-02-24 17:23:42 -0300

    Refatoramento do código

--- src/listaligada/ListaLigada.java
+++ src/listaligada/ListaLigada.java
@@ -4,95 +4,88 @@
  */
 package listaligada;
 
-
 /**
  * @author alexf
  */
 class ListaLigada {
+
     No primeiro, ultimo;
-    
-    ListaLigada(){
-       primeiro = null;
-       ultimo = null;
-    }
-    
-    public boolean ListaVazia(){
-        
-        if (primeiro == null && ultimo == null)
-        {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    
-    
-    public void Destruir(){
-        
+
+    ListaLigada() {
         primeiro = null;
         ultimo = null;
     }
-    
-    public int ContarNos(){
-        
+
+    public boolean ListaVazia() {
+
+        if (primeiro == null && ultimo == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void Destruir() {
+
+        primeiro = null;
+        ultimo = null;
+    }
+
+    public int ContarNos() {
+
         int tamanho = 0;
         No NoTemp = primeiro;
-        
-        while (NoTemp != null){
+
+        while (NoTemp != null) {
             tamanho = tamanho + 1;
             NoTemp = NoTemp.prox;
         }
         return tamanho;
     }
-    
-    public void MostraLista(){
+
+    public void MostraLista() {
         int i = 1;
         No NoTemp = primeiro;
-        
-        while (NoTemp != null){
-            
+
+        while (NoTemp != null) {
+
             System.out.println("Elemento " + NoTemp.elemento + " posição " + i);
-            
+
             NoTemp = NoTemp.prox;
             i = i + 1;
         }
     }
-    
-    public void ElementoInicio(){
-        
-        if (! ListaVazia()) {
+
+    public void ElementoInicio() {
+
+        if (!ListaVazia()) {
             System.out.println("Lista Ligada Vazia");
         }
     }
-    
-    public void ElementoFinal(){
-        
-        if (! ListaVazia()) {
+
+    public void ElementoFinal() {
+
+        if (!ListaVazia()) {
             System.out.println("O último elemento é " + ultimo.elemento);
-        }
-        else {
+        } else {
             System.out.println("Lista Ligada Vazia");
         }
     }
-    
-    public void InserirInicio (No novoNo){
-        
+
+    public void InserirInicio(No novoNo) {
+
         if (ListaVazia()) {
             ultimo = novoNo;
-        }
-        else {
+        } else {
             novoNo.prox = primeiro;
         }
         primeiro = novoNo;
     }
-    
-    public void InserirFinal (No novoNo){
+
+    public void InserirFinal(No novoNo) {
         if (ListaVazia()) {
             primeiro = novoNo;
-        }
-        else
-        {
+        } else {
             ultimo.prox = novoNo;
         }
         ultimo = novoNo;
