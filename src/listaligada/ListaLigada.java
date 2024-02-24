94b37b45ae635266882fbae8a58b180ac44b5423
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2024-02-24 16:46:04 -0300

    finalized data structure project

---
+++ src/listaligada/ListaLigada.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listaligada;
+
+
+/**
+ * @author alexf
+ */
+class ListaLigada {
+    No primeiro, ultimo;
+    
+    ListaLigada(){
+       primeiro = null;
+       ultimo = null;
+    }
+    
+    public boolean ListaVazia(){
+        
+        if (primeiro == null && ultimo == null)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    
+    public void Destruir(){
+        
+        primeiro = null;
+        ultimo = null;
+    }
+    
+    public int ContarNos(){
+        
+        int tamanho = 0;
+        No NoTemp = primeiro;
+        
+        while (NoTemp != null){
+            tamanho = tamanho + 1;
+            NoTemp = NoTemp.prox;
+        }
+        return tamanho;
+    }
+    
+    public void MostraLista(){
+        int i = 1;
+        No NoTemp = primeiro;
+        
+        while (NoTemp != null){
+            
+            System.out.println("Elemento " + NoTemp.elemento + " posição " + i);
+            
+            NoTemp = NoTemp.prox;
+            i = i + 1;
+        }
+    }
+    
+    public void ElementoInicio(){
+        
+        if (! ListaVazia()) {
+            System.out.println("Lista Ligada Vazia");
+        }
+    }
+    
+    public void ElementoFinal(){
+        
+        if (! ListaVazia()) {
+            System.out.println("O último elemento é " + ultimo.elemento);
+        }
+        else {
+            System.out.println("Lista Ligada Vazia");
+        }
+    }
+    
+    public void InserirInicio (No novoNo){
+        
+        if (ListaVazia()) {
+            ultimo = novoNo;
+        }
+        else {
+            novoNo.prox = primeiro;
+        }
+        primeiro = novoNo;
+    }
+    
+    public void InserirFinal (No novoNo){
+        if (ListaVazia()) {
+            primeiro = novoNo;
+        }
+        else
+        {
+            ultimo.prox = novoNo;
+        }
+        ultimo = novoNo;
+    }
+}
